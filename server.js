f12f74f4778388baaab278c64320192332fbca6c
Rohith Cheloor <rohithcm101@gmail.com>
2022-11-22 01:42:02 -0500

    Prod

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const path = require("path");
-const port = process.env.PORT || 8080;
-const app = express();
+var path = require("path");
+var express = require("express");
 
-app.use(express.static(__dirname));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public/index.html"));
+var app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.set("port", process.env.PORT || 8080);
+
+var server = app.listen(app.get("port"), function () {
+  console.log("listening on port ", server.address().port);
 });
-app.listen(port);
-console.log(`Listening on port : ${port}`);
