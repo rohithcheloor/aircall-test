4bfc4dfe507e4f9f3b510b188c2c140655fecb99
Rohith Cheloor <rohithcm101@gmail.com>
2022-11-22 01:28:39 -0500

    Prod test

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Inbox from "../pages/inbox/Inbox.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+
+const Details = ({ item }) => {
+  return <div></div>;
+};
+
+export default Details;
