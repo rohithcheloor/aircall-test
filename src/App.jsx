d202d2d23842d7c0a559af7bfd64eea312c3d347
Rohith Cheloor <rohithcm101@gmail.com>
2022-11-22 11:35:20 -0500

    Added Details Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 
 import Header from "./components/Header.jsx";
 import PageRouter from "./components/PageRouter.jsx";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(0);
+  const [prevPage, setPrevPage] = useState(0);
+  const changePage = (page) => {
+    setPrevPage(currentPage);
+    setCurrentPage(page);
+  };
   return (
     <div className="container p-0">
-      <Header currentPage={currentPage} changePage={setCurrentPage} />
+      <Header currentPage={currentPage} changePage={changePage} />
       <div className="container-view">
-        <PageRouter currentPage={currentPage} />
+        <ToastContainer />
+        <PageRouter
+          currentPage={currentPage}
+          prevPage={prevPage}
+          changePage={changePage}
+        />
       </div>
     </div>
   );
